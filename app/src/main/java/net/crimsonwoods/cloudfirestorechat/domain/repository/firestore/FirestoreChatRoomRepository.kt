71bcb8f20acec999dae659541d5060d938bbea56
crimsonwoods <crimsonwoods.dev@gmail.com>
2020-01-29 22:04:19 +0900

    Update sample data

--- app/src/main/java/net/crimsonwoods/cloudfirestorechat/domain/repository/firestore/FirestoreChatRoomRepository.kt
+++ app/src/main/java/net/crimsonwoods/cloudfirestorechat/domain/repository/firestore/FirestoreChatRoomRepository.kt
@@ -3,8 +3,7 @@ package net.crimsonwoods.cloudfirestorechat.domain.repository.firestore
 import com.google.firebase.firestore.FirebaseFirestore
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
-import net.crimsonwoods.cloudfirestorechat.domain.ChatRoom
-import net.crimsonwoods.cloudfirestorechat.domain.ChatRoomId
+import net.crimsonwoods.cloudfirestorechat.domain.*
 import net.crimsonwoods.cloudfirestorechat.domain.repository.ChatRoomRepository
 import javax.inject.Inject
 
@@ -18,6 +17,13 @@ class FirestoreChatRoomRepository @Inject constructor(
 
     override fun updates(id: ChatRoomId): Observable<ChatRoom> {
         // TODO implement
-        return Observable.just(ChatRoom(id, listOf(), listOf()))
+        return Observable.just(ChatRoom(id, listOf(
+            User(UserId("U0001"), "0001"),
+            User(UserId("U0002"), "0002")
+        ), listOf(
+            Message.OtherMessage(UserId("U0002"), "A", 0L),
+            Message.OtherMessage(UserId("U0002"), "B", 1L),
+            Message.SelfMessage(UserId("U0001"), "テスト", 2L)
+        )))
     }
 }
