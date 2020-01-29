74f899673eb60d211b8afafe2e97a9bdd52344d7
crimsonwoods <crimsonwoods.dev@gmail.com>
2020-01-29 23:33:16 +0900

    Implements Repositories

--- app/src/main/java/net/crimsonwoods/cloudfirestorechat/domain/repository/firestore/FirestoreChatRoomRepository.kt
+++ app/src/main/java/net/crimsonwoods/cloudfirestorechat/domain/repository/firestore/FirestoreChatRoomRepository.kt
@@ -1,29 +1,82 @@
 package net.crimsonwoods.cloudfirestorechat.domain.repository.firestore
 
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.disposables.Disposable
 import net.crimsonwoods.cloudfirestorechat.domain.*
 import net.crimsonwoods.cloudfirestorechat.domain.repository.ChatRoomRepository
+import java.util.concurrent.atomic.AtomicBoolean
 import javax.inject.Inject
 
 class FirestoreChatRoomRepository @Inject constructor(
     private val firestore: FirebaseFirestore
 ) : ChatRoomRepository {
     override fun updates(id: ChatRoomId): Observable<ChatRoom> {
-        // TODO implement
-        return Observable.just(ChatRoom(id, listOf(
-            User(UserId("U0001"), "0001"),
-            User(UserId("U0002"), "0002")
-        ), listOf(
-            Message.OtherMessage(UserId("U0002"), "A", 0L),
-            Message.OtherMessage(UserId("U0002"), "B", 1L),
-            Message.SelfMessage(UserId("U0001"), "テスト", 2L)
-        )))
+        return Observable.create { emitter ->
+            firestore.document("rooms/$id")
+                .get()
+                .addOnSuccessListener {
+                    if (it == null || !it.exists()) {
+                        // TODO ChatRoomが削除された？
+                        return@addOnSuccessListener
+                    }
+
+                    val members = (it["members"] as? List<*>)?.map { userId ->
+                        UserId(userId as String)
+                    } ?: listOf()
+
+                    firestore.collection("rooms/$id/messages")
+                        .orderBy("time_stamp")
+                        .addSnapshotListener { messages, e ->
+                            if (e != null) {
+                                emitter.onError(e)
+                                return@addSnapshotListener
+                            }
+
+                            if (messages == null) {
+                                return@addSnapshotListener
+                            }
+
+                            emitter.onNext(ChatRoom(id, members, messages.documents.map { message ->
+                                val userId = UserId(message["user_id"] as String)
+                                val messageText = message["message"] as String
+                                val timeStamp = message["time_stamp"] as Long
+                                if (userId == UserId("U0001")) {
+                                    Message.SelfMessage(userId, messageText, timeStamp)
+                                } else {
+                                    Message.OtherMessage(userId, messageText, timeStamp)
+                                }
+                            }))
+                        }
+                }
+                .addOnFailureListener {
+                    emitter.onError(it)
+                }
+        }
     }
 
     override fun postMessage(id: ChatRoomId, message: Message): Completable {
-        return Completable.complete()
+        return Completable.create { emitter ->
+            firestore.collection("rooms/$id/messages")
+                .add(mapOf(
+                    "user_id" to message.ownerUserId.toString(),
+                    "message" to message.message,
+                    "time_stamp" to message.timeStamp
+                ))
+                .apply {
+                    addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            emitter.onComplete()
+                        } else {
+                            it.exception?.run { emitter.onError(this) }
+                        }
+                    }
+                }
+        }
     }
 }
--- app/src/main/java/net/crimsonwoods/cloudfirestorechat/domain/repository/firestore/FirestoreUserRepository.kt
+++ app/src/main/java/net/crimsonwoods/cloudfirestorechat/domain/repository/firestore/FirestoreUserRepository.kt
@@ -2,7 +2,9 @@ package net.crimsonwoods.cloudfirestorechat.domain.repository.firestore
 
 import com.google.firebase.firestore.FirebaseFirestore
 import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.disposables.Disposable
 import net.crimsonwoods.cloudfirestorechat.domain.User
+import net.crimsonwoods.cloudfirestorechat.domain.UserIcon
 import net.crimsonwoods.cloudfirestorechat.domain.UserId
 import net.crimsonwoods.cloudfirestorechat.domain.repository.UserRepository
 import javax.inject.Inject
@@ -11,7 +13,19 @@ class FirestoreUserRepository @Inject constructor(
     private val firestore: FirebaseFirestore
 ) : UserRepository {
     override fun get(id: UserId): Single<User> {
-        // TODO implement
-        return Single.just(User(id, "$id"))
+        return Single.create<User> { emitter ->
+            firestore.document("users/$id")
+                .get()
+                .apply {
+                    addOnSuccessListener { user ->
+                        val name = user["name"] as String
+                        val icon = user["icon"] as String
+                        emitter.onSuccess(User(id, name, UserIcon.from(icon)))
+                    }
+                    addOnFailureListener {
+                        emitter.onError(it)
+                    }
+                }
+        }
     }
 }
