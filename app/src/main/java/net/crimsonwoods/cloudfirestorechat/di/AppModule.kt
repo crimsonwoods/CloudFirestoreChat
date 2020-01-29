593fc97ae0bf7681e306f80752c7946b573a002a
crimsonwoods <crimsonwoods.dev@gmail.com>
2020-01-29 19:26:40 +0900

    Provides instances of Repositories

--- app/src/main/java/net/crimsonwoods/cloudfirestorechat/di/AppModule.kt
+++ app/src/main/java/net/crimsonwoods/cloudfirestorechat/di/AppModule.kt
@@ -1,7 +1,25 @@
 package net.crimsonwoods.cloudfirestorechat.di
 
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
+import dagger.Provides
+import net.crimsonwoods.cloudfirestorechat.domain.repository.ChatRoomRepository
+import net.crimsonwoods.cloudfirestorechat.domain.repository.UserRepository
+import net.crimsonwoods.cloudfirestorechat.domain.repository.firestore.FirestoreChatRoomRepository
+import net.crimsonwoods.cloudfirestorechat.domain.repository.firestore.FirestoreUserRepository
+import javax.inject.Singleton
 
 @Module
 class AppModule {
+    @Singleton
+    @Provides
+    fun providesFirestore(): FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    @Singleton
+    @Provides
+    fun provideChatRoomRepository(impl: FirestoreChatRoomRepository): ChatRoomRepository = impl
+
+    @Singleton
+    @Provides
+    fun provideUserRepository(impl: FirestoreUserRepository): UserRepository = impl
 }
